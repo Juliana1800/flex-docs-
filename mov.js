bbb976eb2c65896d3991bc1ec7deafa7f55a3100
Juliana Ritomi Tokura <141957877+Juliana1800@users.noreply.github.com>
2023-11-25 20:38:37 +0000

    feat: animação de contido

--- mov.js
+++ mov.js
@@ -1,3 +1,11 @@
-function index () {
-    
-}
+// function mov () {
+// }  window.addEventListener('scroll', function() {
+//     var elemento = document.querySelector('.conteudo1');
+//     var posicaoDoElemento = elemento.getBoundingClientRect().top;
+//     var alturaDaJanela = window.innerHeight;
+
+//     // Adiciona a classe .animar quando o elemento estiver visível na janela
+//     if (posicaoDoElemento - alturaDaJanela <= 0) {
+//       elemento.classList.add('animar');
+//     }
+//   });
