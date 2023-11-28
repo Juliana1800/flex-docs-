2045a4f7402964125c143d3468914aa988f78ada
Juliana Ritomi Tokura <141957877+Juliana1800@users.noreply.github.com>
2023-11-28 13:54:47 +0000

    feat:arquivo

--- mov.js
+++ mov.js
@@ -1,14 +1,16 @@
-// function mov () {
-// }  window.addEventListener('scroll', function() {
-//     var elemento = document.querySelector('.conteudo1');
-//     var posicaoDoElemento = elemento.getBoundingClientRect().top;
-//     var alturaDaJanela = window.innerHeight;
+function mov () {
+}  window.addEventListener('scroll', function() {
+    var elemento = document.querySelector('#conteudo1');
+    var posicaoDoElemento = elemento.getBoundingClientRect().top;
+    var alturaDaJanela = window.innerHeight;
 
-//     // Adiciona a classe .animar quando o elemento estiver visível na janela
-//     if (posicaoDoElemento - alturaDaJanela <= 0) {
-//       elemento.classList.add('animar');
-//     }
-//   });
+    // Adiciona a classe .animar quando o elemento estiver visível na janela
+    if (posicaoDoElemento - alturaDaJanela <= 0) {
+      elemento.classList.add('conteudo1');
+    } else {
+      elemento.classList.remove('conteudo1');
+    }
+  });
 
 // JQuery
 // $(window).scroll(function() {
